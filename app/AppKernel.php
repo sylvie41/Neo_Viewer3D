d72031ae9ca555e8ef404d529eef77348051b964
sylvie41 <retatsylvie@gmail.com>
2017-03-06 12:02:45 +0100

    Dropzone

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new ViewerBundle\ViewerBundle(),
-        );
+            new Oneup\UploaderBundle\OneupUploaderBundle(),);
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
