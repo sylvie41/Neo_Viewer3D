bf303198f54642a22c45bb85564b3c7a3ea949f1
Myystigri <dorian.walck@gmail.com>
2017-03-13 15:39:47 +0100

    add validate option

--- src/ViewerBundle/Controller/GestionController.php
+++ src/ViewerBundle/Controller/GestionController.php
@@ -3,6 +3,9 @@
 namespace ViewerBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use ViewerBundle\Entity\Folder;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class GestionController extends Controller
 {
@@ -14,6 +17,30 @@ class GestionController extends Controller
         return $this->render('ViewerBundle:Gestion:index_cabinet.html.twig', array(
             'folders' => $folders,
         ));
+    }
+
+    public function validateFolderAction(Folder $folder, Request $request) {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $folderId = $folder->getId();
+        $state = $request->query->get('state');
+
+        if ($state == 'checked') {
+            $folder->setAccepted(true);
+        } else {
+            $folder->setAccepted(false);
+        }
+
+        $em->persist($folder);
+        $em->flush();
+
+        $response = new JsonResponse();
+        $response->setData(array(
+            'folderId' => $folderId,
+        ));
+
+        return $response;
     }
 
 }
